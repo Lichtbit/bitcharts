833a1b2800e91530f6b7474ffdac60068cb5ff83
Sebastian Gaul <sebastian.gaul@lichtbit.com>
2016-07-17 18:07:00 +0200

    Preload charts in Development mode

--- app/controllers/bitcharts/application_controller.rb
+++ app/controllers/bitcharts/application_controller.rb
@@ -1,4 +1,24 @@
 module Bitcharts
   class ApplicationController < ActionController::Base
+    before_action :load_charts unless Rails.configuration.eager_load
+
+    def load_paths
+      [File.expand_path('app/charts', Rails.root)]
+    end
+
+    def load_charts
+      unless loaded?
+        files.each{ |file| require_dependency(file) }
+        @@loaded = true
+      end
+    end
+
+    def files
+      load_paths.flatten.compact.uniq.flat_map{ |path| Dir["#{path}/**/*.rb"] }
+    end
+
+    def loaded?
+      Rails.configuration.eager_load || (@@loaded ||= false)
+    end
   end
 end
--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -4,8 +4,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def show
-    # Preload charts for development mode
-    DayChart
-    DayCreationChart
   end
 end
