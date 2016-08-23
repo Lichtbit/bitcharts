84a639a4e5229b62dab159392187a5afb9370eb9
Sebastian Gaul <sebastian.gaul@lichtbit.com>
2016-08-23 11:00:00 +0200

    Add helpers for canvas elements

--- app/helpers/bitcharts/application_helper.rb
+++ app/helpers/bitcharts/application_helper.rb
@@ -1,4 +1,20 @@
 module Bitcharts
   module ApplicationHelper
+    def bitchart(keys, from: 7.days.ago, to: 0.days.ago, interval: '1 day', type: 'line', colors: nil, **options)
+      keys     = keys.join(' ')   if keys.respond_to?(:join)
+      from     = from.to_date     if from.respond_to?(:to_date)
+      to       = to.to_date       if to.respond_to?(:to_date)
+      interval = interval.inspect if interval.is_a?(ActiveSupport::Duration)
+      colors   = colors.join(' ') if keys.respond_to?(:join)
+      data     = {
+        bitchart:          keys,
+        bitchart_type:     type,
+        bitchart_from:     from.try(:to_s),
+        bitchart_to:       to.try(:to_s),
+        bitchart_interval: interval,
+        bitchart_colors:   colors,
+      }
+      tag(:canvas, options.merge(data: data.compact))
+    end
   end
 end
--- lib/bitcharts/engine.rb
+++ lib/bitcharts/engine.rb
@@ -2,5 +2,8 @@ module Bitcharts
   class Engine < ::Rails::Engine
     isolate_namespace Bitcharts
     config.autoload_paths += %W(#{config.root}/app/charts)
+    config.to_prepare do
+      ::ApplicationController.helper(Bitcharts::ApplicationHelper)
+    end
   end
 end
